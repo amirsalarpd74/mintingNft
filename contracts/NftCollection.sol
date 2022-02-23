92ee1e1b5bb9ed7df4780b0728c1b8b99b05cf5b
amirsalarpd74 <amirsalar.p.d74@gmail.com>
2022-02-23 09:35:49 +0330

    add has_mandotary_fields function.

--- contracts/NftCollection.sol
+++ contracts/NftCollection.sol
@@ -2,6 +2,7 @@ pragma solidity ^0.6.0;
 
 import "./NftMinting.sol";
 
+// NFT type define 
 library NftLibs {
     /*
      *  Store a reference to an image, stored in IPFS, in an ethereum smart contract.
@@ -29,4 +30,20 @@ contract NftCollection {
      */
     mapping (address => NftLibs.NftInfo[]) AddressToNftInfos;
 
+    function create_nft(string memory _name, address _address, string memory _ipfsInfo)
+            public has_mandotary_fields(_name, _address, _ipfsInfo) {
+        
+    NftLibs.NftInfo memory nftInfo(_name, _address, NftLibs.ImageInfo(_ipfsInfo), NftLibs.Price(0))
+        
+    }
+
+    modifier has_mandotary_fields(string memory _name, address _address, string memory _ipfsInfo) {
+        bytes memory tempEmptyNameStringTest = bytes(_name); 
+        bytes memory tempEmptyImageStringTest = bytes(_ipfsInfo); 
+
+        require(tempEmptyNameStringTest.length > 0 
+                && tempEmptyImageStringTest.length > 0
+                && _address != address(0));
+        _;
+    }
 }
